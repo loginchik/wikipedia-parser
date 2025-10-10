b6eeabbb18a1fdc39ca3d914318a9a1382501723
Anna Loginova <zv1376a@gmail.com>
2025-10-11 01:34:09 +0300

    feat: update init in wikimedia parser

---
+++ src/wikimedia_parser/__init__.py
@@ -0,0 +1,5 @@
+from .parser import WikimediaParser
+from .types import WikimediaRequest, PageStatistics
+from .enums import DateGranularity, UserAgent, AccessType
+
+__all__ = ['WikimediaParser', 'WikimediaRequest', 'DateGranularity', 'UserAgent', 'AccessType']
